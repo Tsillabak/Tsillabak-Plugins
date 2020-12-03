01828cc5c88f17b76c5ae3f59af41a5c2e7db6b5
Darwin <darwinmackay@gmail.com>
2020-12-03 04:53:07 -0800

    RuneMiner-1.0.0. added files and bootstraped it finally.

---
+++ RuneMiner/src/main/java/net/runelite/client/plugins/RuneMiner/RuneMinerState.java
@@ -0,0 +1,17 @@
+package net.runelite.client.plugins.RuneMiner;
+
+public enum RuneMinerState
+{
+	ANIMATING,
+	MOVING,
+	TIMEOUT,
+	FIND_BANK,
+	DEPOSIT_ITEMS,
+	WALK_TO_MINE,
+	MINE_ESSENCE,
+	CLICKING_PORTAL,
+	CLICKING_NPC_PORTAL,
+	CLICK_AUBURY,
+	ITERATING,
+	IDLE
+}
