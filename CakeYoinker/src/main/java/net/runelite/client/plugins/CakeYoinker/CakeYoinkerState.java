42fd696fdd5cb6c24d2ca4cd7dee182698cc4046
Darwin <darwinmackay@gmail.com>
2020-12-20 08:14:06 -0800

    Changed the name of RuneMiner to EssenceMiner

---
+++ CakeYoinker/src/main/java/net/runelite/client/plugins/CakeYoinker/CakeYoinkerState.java
@@ -0,0 +1,13 @@
+package net.runelite.client.plugins.CakeYoinker;
+
+public enum CakeYoinkerState
+{
+	ANIMATING,
+	MOVING,
+	TIMEOUT,
+	FIND_BANK,
+	DEPOSIT_ITEMS,
+	WALK_TO_STALL,
+	YOINK_CAKES,
+	IDLE
+}
