af71d6f6edf9f1ecb4c797984b4830c48935577d
Kaballist <Darwinmackay@gmail.com>
2021-03-05 19:07:20 -0800

    V4 fix

--- EssenceMiner/EssenceMiner.gradle.kts
+++ EssenceMiner/EssenceMiner.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "2.0.1"
+version = "2.0.2"
 
 project.extra["PluginName"] = "EssenceMiner"
 project.extra["PluginDescription"] = "Automatically Mines  Essence"
--- FruitCollector/FruitCollector.gradle.kts
+++ FruitCollector/FruitCollector.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "2.0.1"
+version = "2.0.2"
 
 project.extra["PluginName"] = "FruitCollector"
 project.extra["PluginDescription"] = "Automatically Steals Fruit"
