38028725cb328f9d36e8e5be34a9f208d34409f5
Kaballist <Darwinmackay@gmail.com>
2021-03-05 18:59:31 -0800

    V4 fix

--- FruitCollector/FruitCollector.gradle.kts
+++ FruitCollector/FruitCollector.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "2.0.0"
+version = "2.0.1"
 
 project.extra["PluginName"] = "FruitCollector"
 project.extra["PluginDescription"] = "Automatically Steals Fruit"
