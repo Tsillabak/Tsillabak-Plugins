95f2abdbd80461295aa3491819e5fce741b13dbe
Kaballist <Darwinmackay@gmail.com>
2021-06-10 23:14:45 -0700

    fixed mistakes and updated

---
+++ EssenceMiner/src/main/java/net/runelite/client/plugins/essenceminer/EssenceMinerState.java
@@ -0,0 +1,18 @@
+package net.runelite.client.plugins.essenceminer;
+
+public enum EssenceMinerState
+{
+	ANIMATING,
+	MOVING,
+	TIMEOUT,
+	FIND_BANK,
+	DEPOSIT_ITEMS,
+	WALK_TO_MINE,
+	MINE_ESSENCE,
+	CLICKING_PORTAL,
+	CLICKING_NPC_PORTAL,
+	CLICK_AUBURY,
+	ITERATING,
+	IDLE
+}
+
